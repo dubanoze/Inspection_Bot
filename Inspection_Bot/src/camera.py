584e22b52399830564582bd14fb6b15428c72a79
richhohart <richhohart@gmail.com>
2014-06-14 13:09:14 -0500

    Adding camera initialization script

---
+++ Inspection_Bot/src/camera.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import cv2.cv as cv
+
+#  --------------------------------------------------------------
+#  Initialize Camera
+#  --------------------------------------------------------------
+
+def camera():
+    cv.NamedWindow("camera", 1)
+    
+    capture = cv.CaptureFromCAM(0)
+    
+    while True:
+        img = cv.QueryFrame(capture)
+        cv.ShowImage("camera", img)
+        if cv.WaitKey(10) == 27: #Esc key to exit
+            cv.SaveImage("../saved_images/sample.png",img)
+            break
+    cv.DestroyAllWindows()
+
+#  --------------------------------------------------------------
+#  Main
+#  Notes:
+#  --------------------------------------------------------------    
+        
+if __name__ == '__main__':
+    camera()
