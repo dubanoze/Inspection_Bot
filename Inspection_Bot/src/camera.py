067440ae24e84bd9284ab63fd1d374fb7764d569
richhohart <richhohart@gmail.com>
2014-06-17 13:51:32 -0500

    GoTo Command is not thread safe

--- Inspection_Bot/src/camera.py
+++ Inspection_Bot/src/camera.py
@@ -8,15 +8,15 @@ import cv2.cv as cv
 
 
 
-
-cv.NamedWindow("camera", 1)
-
-capture = cv.CaptureFromCAM(0)
-
-while True:
-    img = cv.QueryFrame(capture)
-    cv.ShowImage("camera", img)
-    if cv.WaitKey(10) == 27: #Esc key to exit
-        cv.SaveImage("../saved_images/sample.png",img)
-        break
-cv.DestroyAllWindows()
+if __name__ == '__main__':
+    cv.NamedWindow("camera", 1)
+    
+    capture = cv.CaptureFromCAM(0)
+    
+    while True:
+        img = cv.QueryFrame(capture)
+        cv.ShowImage("camera", img)
+        if cv.WaitKey(10) == 27: #Esc key to exit
+            cv.SaveImage("../saved_images/sample.png",img)
+            break
+    cv.DestroyAllWindows()
