06494f67a5f46efd5828e04d4dfbeb66e7390362
richhohart <richhohart@gmail.com>
2014-07-16 11:19:58 -0500

    commenting chip analysis code

--- Inspection_Bot/chip_analysis/component_analysis.py
+++ Inspection_Bot/chip_analysis/component_analysis.py
@@ -4,24 +4,25 @@ import time
 import os
 import numpy as np
 
-
+#return all the pixel in an image as a 
+# hue saturation and value or HSV list. 
 def get_pixel_list(img):
     pixels = []
-    
-    #print img[1,1]
-    #img = np.asarray(img[:,:,:])
     height, width, depth = img.shape
     for i in range(0, height):
         for j in range(0, width):
             pixel_value = img[ i, j]
-            # Since OpenCV loads color images in BGR, not RGB
             h = pixel_value[0]
             s = pixel_value[1]
             v = pixel_value[2]
             pixels.append([h,s,v])
     return pixels
 
-#see http://en.wikipedia.org/wiki/Mean_of_circular_quantities
+#calculate the average hue of the pixels in a list.  Notice that
+# this requires a specialized calculation because the HSV color scale 
+# is different from the traditional RBG color scale. 
+#see http://en.wikipedia.org/wiki/Mean_of_circular_quantities and 
+# http://en.wikipedia.org/wiki/HSL_and_HSV for more information
 def get_hue_mean(pixels):
     hue_list =[]
     
@@ -31,8 +32,11 @@ def get_hue_mean(pixels):
     hue_mean =np.arctan2(np.mean(np.sin(hue_list)),np.mean(np.cos(hue_list)))
     hue_mean=np.degrees(hue_mean)
     return hue_mean
-    #first_arg = 0
-    #second_arg = 0
+
+
+# calculate the standard deviation of the hue of the pixels.  Methodology for 
+# calculating the standard deviation is similar to the methodology for calculating
+# the mean of the pixels. 
 def get_hue_std(pixels):
     hue_list =[]
     
@@ -42,10 +46,18 @@ def get_hue_std(pixels):
     hue_std =np.arctan2(np.std(np.sin(hue_list)),np.std(np.cos(hue_list)))
     hue_std=np.degrees(hue_std)
     return hue_std
-    
 
+# This script takes all the images in the similar_cropped_parts folder
+# and returns the statistical min and max pixel ranges of those files.
+# These values are used by the Camera->threshold_camera.py script to 
+# differenciate different components from one another. 
 if __name__ == '__main__':
+    # directory holding the collection of images to be analysed.  Right now
+    # the similar_cropped_parts directory holds images of the center of 
+    # capasitors held on the rambo board. 
     directory_path = './similar_cropped_parts/'
+    
+    
     part_file_names = os.listdir(directory_path)
     pixels=[]
     for part in part_file_names:
@@ -53,22 +65,23 @@ if __name__ == '__main__':
         img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         pixels=pixels+get_pixel_list(img)
     
+    #find the mean and standard deviation of the pixels returned. 
+    #Some of these calcualtions are inaccurate
     pixel_mean= np.mean(pixels, axis=0)
     pixel_standard_deviation= np.std(pixels,axis=0)    
-    #pixel_max = np.amax(pixels,axis=0)
-    #pixel_min = np.amin(pixels,axis=0)
-    #print pixel_mean
-    #print pixel_standard_deviation
-    #print pixel_max
+    
+    #replace the inaccurate calculate of the hue pixel average 
     hue_mean =  get_hue_mean(pixels)
     hue_std=  get_hue_std(pixels)
     #hue_min = 
-    factor = 4
+    factor = 3
     pixel_min = [get_hue_mean(pixels) - factor*get_hue_std(pixels),pixel_mean[1]-factor*pixel_standard_deviation[1],pixel_mean[2]-factor*pixel_standard_deviation[2]]
-    print pixel_min
+    print "Estimated pixel minimum "+str(pixel_min)
     pixel_max = pixel_min = [get_hue_mean(pixels) + factor*get_hue_std(pixels),pixel_mean[1]+factor*pixel_standard_deviation[1],pixel_mean[2]+factor*pixel_standard_deviation[2]]
-    print pixel_max
-    #print "pixel max = ({0:f},{1:f},{2:f})".format(float(pixel_max[0],pixel_max[1],pixel_max[2]))
-    #while True:
-        #k = cv2.waitKey(0)&0xFF
-        #if k == ord('a'):
+    print "Estimated pixel maximum "+str(pixel_max)
+    
+
+
+
+    
+
