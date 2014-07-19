739c9619a9a3ab9641c1a9c51d60d78f9c6a5aad
richhohart <richhohart@gmail.com>
2014-07-18 23:18:03 -0500

    adding images to documentation

--- Inspection_Bot/Volume_Data/pixel_selection.py
+++ Inspection_Bot/Volume_Data/pixel_selection.py
@@ -6,6 +6,14 @@ import numpy as np
 import math
 if __name__ == '__main__':
     #directory='data/bot_movement_samples/'
+    laser_image_directory = 'data/chip_laser_photos_2/'
+    laser_image_file_names = os.listdir(laser_image_directory)
+    laser_image_file_names.sort()
+    
+    led_image_directory = 'data/Chip_led_photos/'
+    led_image_file_names = os.listdir(led_image_directory)
+    led_image_file_names.sort()
+    
     laser_data_directory='data/laser_binary_despec/'
     laser_file_names = os.listdir(laser_data_directory)
     laser_file_names.sort()
@@ -15,9 +23,15 @@ if __name__ == '__main__':
     chip_file_names.sort()
     #X_pixel_positions=[]
     #Y_pixel_positions=[]
+    print len(laser_image_file_names)
+    print len(led_image_file_names)
     print len(laser_file_names)
     print len(chip_file_names)
     for i in range(0, len(chip_file_names)):
+        l_i_img = cv2.imread(laser_image_directory+laser_image_file_names[i])
+        c_i_img = cv2.imread(led_image_directory+led_image_file_names[i])
+        
+        
         c_img = cv2.imread(chip_data_directory+chip_file_names[i])
         l_img = cv2.imread(laser_data_directory+laser_file_names[i])
         
@@ -30,6 +44,8 @@ if __name__ == '__main__':
         selected_pixels =cv2.bitwise_and(c_dst,l_dst)
         
         while True:
+            cv.ShowImage("raw laser photos",cv.fromarray(l_i_img))
+            cv.ShowImage("chip raw image with leds",cv.fromarray(c_i_img))
             cv.ShowImage("laser pixels", cv.fromarray(l_dst))
             cv.ShowImage("component pixels", cv.fromarray(c_dst))
             cv.ShowImage("selected pixels", cv.fromarray(selected_pixels))
